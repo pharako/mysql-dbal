695c7398da6e301f91e04cf037e1ccbb8b96d809
claudiobizzotto <it@claudiobizzotto.com>
2020-01-11 16:25:38 -0500

    Remove duplicate method

--- src/Connection.php
+++ src/Connection.php
@@ -84,17 +84,6 @@ class Connection extends DBALConnection implements DriverConnection
         return $this->executeUpdate($sql, $params, $finalTypes);
     }
 
-    private function extractTypeValues(array $columnList, array $types)
-    {
-        $typeValues = [];
-
-        foreach ($columnList as $columnIndex => $columnName) {
-            $typeValues[] = $types[$columnIndex] ?? ParameterType::STRING;
-        }
-
-        return $typeValues;
-    }
-
     /**
      * Executes a ON DUPLICATE KEY UPDATE statement on a MySQL table.
      *
